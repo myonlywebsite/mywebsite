1f6eae63a004a46989ffc3b70a425923cb7c80f6
myonlywebsite <120015518+myonlywebsite@users.noreply.github.com>
2022-12-07 11:45:07 +0100

    Add files via upload

---
+++ mvc/src/main/java/com/example/mvc/config/MvcConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="com.example.mvc")
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer{
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".html");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("file:///C:/Users/AlgertShevroja/Desktop/mvc-repository/mvc/");
+	}
+
+	
+}
